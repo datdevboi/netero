63eca406de2d75022d0da9acac0712560281d864
camilo17 <cami172013@gmail.com>
2018-08-22 17:40:01 -0500

    Made an Example Made a quick example showing off our buttons

--- packages/app/src/routes/Buttons.tsx
+++ packages/app/src/routes/Buttons.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { Button } from "netero-ui";
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live";
 import { PropTable } from "../components/PropTable";
 
 export class Buttons extends React.Component {
@@ -8,11 +9,47 @@ export class Buttons extends React.Component {
   };
 
   public render() {
+    const example = `
+    <div style={{ height: "100px", display: 'flex', justifyContent: 'space-around', flexWrap: 'wrap', alignItems: "end"}}>
+      <Button type="Primary" onClick={this.onClick}>
+        Primary
+      </Button>
+      <Button type="Info" onClick={this.onClick}>
+        Info
+      </Button>
+      <Button type="Success" onClick={this.onClick}>
+        Success
+      </Button>
+      <Button type="Danger" onClick={this.onClick}>
+        Danger
+      </Button>
+      <Button type="Warning" onClick={this.onClick}>
+        Warning
+      </Button>
+      <Button color="palevioletred" onClick={this.onClick}>
+        Custom Color
+      </Button>
+    </div>
+    `;
     return (
-      <div>
-        <Button type="Primary" onClick={this.onClick}>
-          Hello
-        </Button>
+      <div
+        style={{
+          position: "relative",
+          display: "flex",
+          height: "100%",
+          flexDirection: "column",
+          justifyContent: "space-between"
+        }}
+      >
+        <LiveProvider scope={{ Button, onClick: this.onClick }} code={example}>
+          <div style={{ marginBottom: "25px" }}>
+            <LivePreview />
+          </div>
+
+          <LiveEditor />
+          <LiveError />
+        </LiveProvider>
+
         <PropTable name="Button" />
       </div>
     );
