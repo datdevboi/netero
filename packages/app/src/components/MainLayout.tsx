f3091bc8d9205b62303c801dd7bca2bdb1c04859
camilo17 <cami172013@gmail.com>
2018-08-21 19:36:31 -0500

    made use of our MenuItems component in our Sider component

--- packages/app/src/components/MainLayout.tsx
+++ packages/app/src/components/MainLayout.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
-import { Layout, Menu, Icon } from "antd";
+import { Layout, Icon } from "antd";
+import { MenuItems } from "./MenuItems";
 
 const { Header, Sider, Content } = Layout;
 
@@ -24,20 +25,7 @@ export class MainLayout extends React.Component<any, any> {
           collapsed={this.state.collapsed}
         >
           <div className="logo" />
-          <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
-            <Menu.Item key="1">
-              <Icon type="user" />
-              <span>nav 1</span>
-            </Menu.Item>
-            <Menu.Item key="2">
-              <Icon type="video-camera" />
-              <span>nav 2</span>
-            </Menu.Item>
-            <Menu.Item key="3">
-              <Icon type="upload" />
-              <span>nav 3</span>
-            </Menu.Item>
-          </Menu>
+          <MenuItems />
         </Sider>
         <Layout>
           <Header style={{ background: "#fff", padding: 0 }}>
