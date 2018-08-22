a270f60bdef6bbe5e9447401ad564ed753bd9a2b
camilo17 <cami172013@gmail.com>
2018-08-21 19:36:12 -0500

    created  MenuItems component that will render a Menu.Item for each component

---
+++ packages/app/src/components/MenuItems.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import { Menu } from "antd";
+import { extractComponentNames } from "../utils/extractComponentNames";
+import { componentData } from "../data/componentData";
+import { Link } from "react-router-dom";
+
+export class MenuItems extends React.Component {
+  private renderItems = () => {
+    const componentNames = extractComponentNames(componentData);
+
+    return componentNames.map((name: string, i: any) => {
+      return (
+        <Menu.Item key={i}>
+          <Link to={`/${name.toLowerCase()}`}>
+            <span>{name}</span>
+          </Link>
+        </Menu.Item>
+      );
+    });
+  };
+  public render() {
+    return (
+      <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
+        {this.renderItems()}
+      </Menu>
+    );
+  }
+}
