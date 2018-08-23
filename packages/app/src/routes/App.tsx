ee876c768d5a49909276609398ea88471fae7ac1
camilo17 <cami172013@gmail.com>
2018-08-23 15:41:54 -0500

    Made a new route for our Inputs page

--- packages/app/src/routes/App.tsx
+++ packages/app/src/routes/App.tsx
@@ -2,11 +2,13 @@ import * as React from "react";
 import { MainLayout } from "../components/MainLayout";
 import { Route, Switch } from "react-router-dom";
 import { Buttons } from "./Buttons";
+import { Inputs } from "./Inputs";
 
 export const App = () => (
   <MainLayout>
     <Switch>
       <Route exact={true} path="/button" component={Buttons} />
+      <Route exact={true} path="/input" component={Inputs} />
     </Switch>
   </MainLayout>
 );
