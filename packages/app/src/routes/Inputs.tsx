d5385a8e805c75b9deccf73b4532e7434df48854
camilo17 <cami172013@gmail.com>
2018-08-23 15:41:42 -0500

    created an Input page

---
+++ packages/app/src/routes/Inputs.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { Input } from "netero-ui";
+
+export class Inputs extends React.Component {
+  public render() {
+    return <Input placeholder="hellow" />;
+  }
+}
