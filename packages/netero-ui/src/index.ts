aed13766a9af03f35670fe6752e950d479b829c9
camilo17 <cami172013@gmail.com>
2018-09-09 19:50:13 -0500

    exported our Card component

--- packages/netero-ui/src/index.ts
+++ packages/netero-ui/src/index.ts
@@ -1,4 +1,5 @@
 import { Button } from "./components/Button/Button";
 import { Input } from "./components/Input/Input";
+import { Card } from "./components/Card/Card";
 
-export { Button, Input };
+export { Button, Input, Card };
