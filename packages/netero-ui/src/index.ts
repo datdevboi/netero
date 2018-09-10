ae4d8ea3de340ddd91c08bf8c6f7a7d4fbe39857
camilo17 <cami172013@gmail.com>
2018-09-09 19:52:51 -0500

    removed the other exports

--- packages/netero-ui/src/index.ts
+++ packages/netero-ui/src/index.ts
@@ -1,5 +1,3 @@
 import { Button } from "./components/Button/Button";
-import { Input } from "./components/Input/Input";
-import { Card } from "./components/Card/Card";
 
-export { Button, Input, Card };
+export { Button };
