bf140e4998db0929df5e17661e5ad9ca129ceb96
camilo17 <cami172013@gmail.com>
2018-09-09 19:51:19 -0500

    createda route for our Card component

---
+++ packages/app/src/routes/Cards.tsx
@@ -0,0 +1,16 @@
+import * as React from "react";
+import { Card } from "netero-ui";
+
+// import { CodeExample } from "../components/CodeExample";
+
+export class Cards extends React.Component {
+  public render() {
+    return <Card />;
+    // const example = `
+    // <div style={{ height: "100px", display: 'flex', justifyContent: 'space-around', flexWrap: 'wrap', alignItems: "end"}}>
+    //   <Card/>
+    // </div>
+    // `;
+    // return <CodeExample code={example} name="Card" scope={{ Card }} />;
+  }
+}
