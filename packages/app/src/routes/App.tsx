a948f1b34da85721a658943ce0c41370d2881d5a
camilo17 <cami172013@gmail.com>
2018-09-09 19:51:35 -0500

    commented our routes for Inputs and Cards

--- packages/app/src/routes/App.tsx
+++ packages/app/src/routes/App.tsx
@@ -2,13 +2,15 @@ import * as React from "react";
 import { MainLayout } from "../components/MainLayout";
 import { Route, Switch } from "react-router-dom";
 import { Buttons } from "./Buttons";
-import { Inputs } from "./Inputs";
+// import { Inputs } from "./Inputs";
+// import { Cards } from "./Cards";
 
 export const App = () => (
   <MainLayout>
     <Switch>
       <Route exact={true} path="/button" component={Buttons} />
-      <Route exact={true} path="/input" component={Inputs} />
+      {/* <Route exact={true} path="/input" component={Inputs} />
+      <Route exact={true} path="/card" component={Cards} /> */}
     </Switch>
   </MainLayout>
 );
