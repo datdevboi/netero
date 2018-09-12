2ecdde4af2a50c13f6677b75f7521b780cae4988
datdevboi <cami172013@gmail.com>
2018-09-11 20:45:27 -0500

    Made some test for our Button

--- packages/netero-ui/src/components/Button/Button.test.tsx
+++ packages/netero-ui/src/components/Button/Button.test.tsx
@@ -1,3 +1,31 @@
-test("Basic", () => {
-  expect(2).toBe(2);
+import React from "react";
+import {
+  render,
+  fireEvent,
+  cleanup,
+  waitForElement
+} from "react-testing-library";
+import { Button } from "./Button";
+
+describe("Button", () => {
+  const myMockOnClick = jest.fn();
+  const { container, getByText } = render(
+    <Button onClick={myMockOnClick}>Login</Button>
+  );
+
+  test("it renders", () => {
+    expect(container).toMatchSnapshot();
+  });
+
+  test("test onClick", () => {
+    fireEvent(
+      getByText("Login"),
+      new MouseEvent("click", {
+        bubbles: true,
+        cancelable: true
+      })
+    );
+
+    expect(myMockOnClick).toHaveBeenCalledTimes(1);
+  });
 });
