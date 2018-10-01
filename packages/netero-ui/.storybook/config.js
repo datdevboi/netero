86c311ce0133a056e992c4914e5a3aa6cdc5f1f1
datdevboi <cami172013@gmail.com>
2018-10-01 18:33:52 -0500

    made use of withInfo

--- packages/netero-ui/.storybook/config.js
+++ packages/netero-ui/.storybook/config.js
@@ -1,9 +1,12 @@
-import { configure } from '@storybook/react';
+import { configure, addDecorator } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../src/stories', true, /.stories.tsx$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+
+addDecorator(withInfo);
 
 configure(loadStories, module);
