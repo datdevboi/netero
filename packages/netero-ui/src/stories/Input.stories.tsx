db500e41470b23421cf52ce18a33a80ff4204f24
datdevboi <cami172013@gmail.com>
2018-10-01 18:34:36 -0500

    made use of withInfo

--- packages/netero-ui/src/stories/Input.stories.tsx
+++ packages/netero-ui/src/stories/Input.stories.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import { linkTo } from "@storybook/addon-links";
+import { withInfo } from "@storybook/addon-info";
 
 import { Input } from "../components/Input/Input";
 
