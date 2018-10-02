8b32c5e2b8ffb8f4e79f00b763cc8d15350f2a21
datdevboi <cami172013@gmail.com>
2018-10-01 19:50:25 -0500

    Made a better example

--- packages/netero-ui/src/stories/Table.stories.tsx
+++ packages/netero-ui/src/stories/Table.stories.tsx
@@ -3,12 +3,26 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import { linkTo } from "@storybook/addon-links";
-import { withInfo } from "@storybook/addon-info";
 
 import { Table } from "../components/Table/Table";
 
 storiesOf("Table", module).add("simple Table", () => (
   <div style={{ padding: "25px" }}>
-    <Table />
+    <Table>
+      <Table.Header>
+        <Table.Row>
+          {["Name", "Age", "Weight", "Height"].map(header => (
+            <Table.Head>{header}</Table.Head>
+          ))}
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        <Table.Row>
+          {["Brian", 25, "125lb", "6'1"].map(data => (
+            <Table.Data>{data}</Table.Data>
+          ))}
+        </Table.Row>
+      </Table.Body>
+    </Table>
   </div>
 ));
