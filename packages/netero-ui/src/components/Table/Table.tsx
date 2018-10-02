696918b1c9c67f5df91ca1285b120c0a734c7589
datdevboi <cami172013@gmail.com>
2018-10-01 19:42:08 -0500

    added more compound components

--- packages/netero-ui/src/components/Table/Table.tsx
+++ packages/netero-ui/src/components/Table/Table.tsx
@@ -9,13 +9,17 @@ const Head = styled.th``;
 
 const Data = styled.td``;
 
+const MainTable = styled.table``;
+const Body = styled.tbody``;
+
 class Table extends React.Component {
   static Header = Header;
   static Row = Row;
   static Head = Head;
   static Data = Data;
+  static Body = Body;
   render() {
-    return <div>Hello from table</div>;
+    return <MainTable>{this.props.children}</MainTable>;
   }
 }
 
