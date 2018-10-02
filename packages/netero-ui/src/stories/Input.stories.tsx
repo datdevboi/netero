61e2a998ab9f7d8eb9c19ae2c54e7b069bd1ae70
datdevboi <cami172013@gmail.com>
2018-10-01 19:16:09 -0500

    removed withInfo from Input

--- packages/netero-ui/src/stories/Input.stories.tsx
+++ packages/netero-ui/src/stories/Input.stories.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import { linkTo } from "@storybook/addon-links";
-import { withInfo } from "@storybook/addon-info";
 
 import { Input } from "../components/Input/Input";
 
