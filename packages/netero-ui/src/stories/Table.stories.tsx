52f2b5e75cea63fdcd3a1f6a0d494e2f10239a29
datdevboi <cami172013@gmail.com>
2018-10-06 19:02:59 -0500

    Made use of our react-live to render our Table

--- packages/netero-ui/src/stories/Table.stories.tsx
+++ packages/netero-ui/src/stories/Table.stories.tsx
@@ -3,26 +3,33 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import { linkTo } from "@storybook/addon-links";
-
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live";
 import { Table } from "../components/Table/Table";
 
-storiesOf("Table", module).add("simple Table", () => (
+const TableExample = `
+  <Table>
+    <Table.Header>
+      <Table.Row>
+        {["Name", "Age", "Weight", "Height"].map(header => (
+          <Table.Head>{header}</Table.Head>
+        ))}
+      </Table.Row>
+    </Table.Header>
+    <Table.Body>
+      <Table.Row>
+        {["Brian", 25, "125lb", "6'1"].map(data => (
+          <Table.Data>{data}</Table.Data>
+        ))}
+      </Table.Row>
+    </Table.Body>
+  </Table>
+`;
+storiesOf("Table", module).add("Table", () => (
   <div style={{ padding: "25px" }}>
-    <Table>
-      <Table.Header>
-        <Table.Row>
-          {["Name", "Age", "Weight", "Height"].map(header => (
-            <Table.Head>{header}</Table.Head>
-          ))}
-        </Table.Row>
-      </Table.Header>
-      <Table.Body>
-        <Table.Row>
-          {["Brian", 25, "125lb", "6'1"].map(data => (
-            <Table.Data styledFunc={() => `color: red`}>{data}</Table.Data>
-          ))}
-        </Table.Row>
-      </Table.Body>
-    </Table>
+    <LiveProvider scope={{ Table }} code={TableExample.toString()}>
+      <LiveEditor />
+      <LiveError />
+      <LivePreview />
+    </LiveProvider>
   </div>
 ));
