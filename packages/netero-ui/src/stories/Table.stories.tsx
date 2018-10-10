622294d12be6e8fa5289117a7dd5ecb19518bc9c
datdevboi <cami172013@gmail.com>
2018-10-09 21:13:17 -0500

    Made use of our PropTable

--- packages/netero-ui/src/stories/Table.stories.tsx
+++ packages/netero-ui/src/stories/Table.stories.tsx
@@ -1,29 +1,28 @@
 import React from "react";
 
 import { storiesOf } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
-import { linkTo } from "@storybook/addon-links";
+
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live";
 import { Table } from "../components/Table/Table";
-
+import { PropTable } from "../utils/components/PropTable";
 const TableExample = `
   <Table>
     <Table.Header>
       <Table.Row>
-        {["Name", "Age", "Weight", "Height"].map(header => (
-          <Table.Head>{header}</Table.Head>
+        {["Name", "Age", "Weight", "Height"].map((header, index) => (
+          <Table.Head key={index}>{header}</Table.Head>
         ))}
       </Table.Row>
     </Table.Header>
     <Table.Body>
       <Table.Row>
         {["Brian", 25, "125lb", "6'1"].map(data => (
-          <Table.Data>{data}</Table.Data>
+          <Table.Data key={data}>{data}</Table.Data>
         ))}
       </Table.Row>
       <Table.Row>
         {["Jackie", 25, "110lb", "5'4"].map(data => (
-          <Table.Data>{data}</Table.Data>
+          <Table.Data key={data}>{data}</Table.Data>
         ))}
       </Table.Row>
     </Table.Body>
@@ -32,19 +31,25 @@ const TableExample = `
 storiesOf("Table", module).add("Table", () => (
   <div style={{ padding: "25px", height: "250px" }}>
     <h1>Table:</h1>
-    <LiveProvider
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-between",
-        height: "100%"
-      }}
-      scope={{ Table }}
-      code={TableExample.toString()}
-    >
-      <LivePreview />
-      <LiveEditor />
-      <LiveError />
-    </LiveProvider>
+    <div>
+      <LiveProvider
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+          height: "100%"
+        }}
+        scope={{ Table }}
+        code={TableExample.toString()}
+      >
+        <LivePreview />
+        <LiveEditor />
+        <LiveError />
+      </LiveProvider>
+    </div>
+    <div>
+      <h2>Props:</h2>
+      <PropTable componentName="Table" />
+    </div>
   </div>
 ));
