8c69c918bf1193e2932bdec363f1e9c0513df737
datdevboi <cami172013@gmail.com>
2018-10-09 21:13:07 -0500

    created a PropTable component that will render our props in our table

---
+++ packages/netero-ui/src/utils/components/PropTable.tsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+import { Table } from "../../components/Table/Table";
+import { findComponentData } from "../findComponentData";
+import { componentData } from "../../data/componentData";
+
+interface IPropTable {
+  componentName: string;
+}
+
+const propKeys = ["name", "description", "defaultValue", "required", "type"];
+
+export class PropTable extends React.Component<IPropTable> {
+  render() {
+    const data = findComponentData(componentData, this.props
+      .componentName as string);
+    const dataKeys = Object.keys(data);
+
+    return (
+      <Table>
+        <Table.Header>
+          <Table.Row>
+            {propKeys.map((propKey, index) => {
+              return <Table.Head key={index}>{propKey}</Table.Head>;
+            })}
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {dataKeys.map((key: any, index: number) => (
+            <Table.Row key={index}>
+              <Table.Data>{data[key].name}</Table.Data>
+              <Table.Data>{data[key].description}</Table.Data>
+              <Table.Data>{data[key].defaultValue}</Table.Data>
+              <Table.Data>{data[key].required ? "true" : "false"}</Table.Data>
+              <Table.Data>{data[key].type.name}</Table.Data>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
+    );
+  }
+}
