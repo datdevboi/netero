6fb3f707ef4fe2ac0faa2eecc66379afe4b72f14
datdevboi <cami172013@gmail.com>
2018-10-10 08:49:40 -0500

    Made CodeExample component

---
+++ packages/netero-ui/src/utils/components/CodeExample.tsx
@@ -0,0 +1,40 @@
+import * as React from "react";
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live";
+import { PropTable } from "./PropTable";
+
+interface ICodeExample {
+  code: string;
+  scope: object;
+  componentName: string;
+}
+
+export class CodeExample extends React.Component<ICodeExample> {
+  public render() {
+    return (
+      <div
+        style={{
+          position: "relative",
+          display: "flex",
+          height: "100%",
+          flexDirection: "column",
+          justifyContent: "start"
+        }}
+      >
+        <LiveProvider
+          scope={this.props.scope}
+          code={this.props.code}
+          style={{ marginBottom: "50px" }}
+        >
+          <div style={{ marginBottom: "25px" }}>
+            <LivePreview />
+          </div>
+
+          <LiveEditor />
+          <LiveError />
+        </LiveProvider>
+
+        <PropTable componentName={this.props.componentName} />
+      </div>
+    );
+  }
+}
