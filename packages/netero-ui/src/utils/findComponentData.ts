b12b3ca1d7aa03d322c25fc7eecac719c5dc71ae
datdevboi <cami172013@gmail.com>
2018-10-16 18:03:54 -0500

    Our applications now needs both the data and examples. I made the findComponentData function return an object with the data and examples

--- packages/netero-ui/src/utils/findComponentData.ts
+++ packages/netero-ui/src/utils/findComponentData.ts
@@ -1,5 +1,8 @@
 export const findComponentData = (data: any, name: string) => {
   const retrievedData = data.filter((x: any) => x.name === name);
 
-  return retrievedData[0].data.props;
+  return {
+    data: retrievedData[0].data.props,
+    examples: retrievedData[0].examples
+  };
 };
